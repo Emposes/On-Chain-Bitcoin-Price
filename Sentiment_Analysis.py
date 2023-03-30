83129b6c53cfb922680c83855380df8233f0e6cd
Emposes <61826198+Emposes@users.noreply.github.com>
2023-03-30 12:07:37 +0300

    Add files via upload

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,190 @@
+# utilities
+import re
+import numpy as np
+import pandas as pd
+# plotting
+import seaborn as sns
+import matplotlib.pyplot as plt
+# nltk
+from nltk.stem import WordNetLemmatizer
+# sklearn
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import confusion_matrix, classification_report
+
+# Importing the dataset
+DATASET_COLUMNS=['target','ids','date','flag','user','text']
+DATASET_ENCODING = "ISO-8859-1"
+df = pd.read_csv('local_path/training.1600000.processed.noemoticon.csv', encoding=DATASET_ENCODING, names=DATASET_COLUMNS)
+df_sent = pd.read_csv('local_path/btc_twets_new.csv')
+data_bitcoin = df_sent[['Tweet','Date Created']]
+data_bitcoin["Date Created"] = pd.to_datetime(data_bitcoin["Date Created"], errors="coerce")
+data_bitcoin["date"] = data_bitcoin["Date Created"].dt.floor('d')
+bitcoin_tweet_number = data_bitcoin.groupby(data_bitcoin.date.dt.date).count()
+data_bitcoin_work = data_bitcoin.groupby('date').head(1000).reset_index(drop=True)
+data_bitcoin_work = data_bitcoin_work.sort_values(by="date")
+data_bitcoin_work["date"] = data_bitcoin_work["date"].dt.strftime('%d/%m/%Y')
+data_bitcoin_work = data_bitcoin
+# Plotting the distribution for dataset.
+ax = df.groupby('target').count().plot(kind='bar', title='Distribution of data',legend=False)
+ax.set_xticklabels(['Negative','Positive'], rotation=0)
+# Storing data in lists.
+text, sentiment = list(df['text']), list(df['target'])
+sns.countplot(x='target', data=df)
+
+data=df[['text','target']]
+data['target'] = data['target'].replace(4,1)
+
+data_pos = data[data['target'] == 1]
+data_neg = data[data['target'] == 0]
+
+data_pos = data_pos.iloc[:int(30000)]
+data_neg = data_neg.iloc[:int(30000)]
+
+dataset = pd.concat([data_pos, data_neg])
+dataset['text']=dataset['text'].str.lower()
+dataset['text'].tail()
+data_bitcoin_work['Tweet']=data_bitcoin['Tweet'].str.lower()
+data_bitcoin_work['Tweet'].tail()
+data_bitcoin_work['text'] = data_bitcoin_work['Tweet']
+
+stopwordlist = ['a', 'about', 'above', 'after', 'again', 'ain', 'all', 'am', 'an',
+             'and','any','are', 'as', 'at', 'be', 'because', 'been', 'before',
+             'being', 'below', 'between','both', 'by', 'can', 'd', 'did', 'do',
+             'does', 'doing', 'down', 'during', 'each','few', 'for', 'from',
+             'further', 'had', 'has', 'have', 'having', 'he', 'her', 'here',
+             'hers', 'herself', 'him', 'himself', 'his', 'how', 'i', 'if', 'in',
+             'into','is', 'it', 'its', 'itself', 'just', 'll', 'm', 'ma',
+             'me', 'more', 'most','my', 'myself', 'now', 'o', 'of', 'on', 'once',
+             'only', 'or', 'other', 'our', 'ours','ourselves', 'out', 'own', 're','s', 'same', 'she', "shes", 'should', "shouldve",'so', 'some', 'such',
+             't', 'than', 'that', "thatll", 'the', 'their', 'theirs', 'them',
+             'themselves', 'then', 'there', 'these', 'they', 'this', 'those',
+             'through', 'to', 'too','under', 'until', 'up', 've', 'very', 'was',
+             'we', 'were', 'what', 'when', 'where','which','while', 'who', 'whom',
+             'why', 'will', 'with', 'won', 'y', 'you', "youd","youll", "youre",
+             "youve", 'your', 'yours', 'yourself', 'yourselves']
+
+STOPWORDS = set(stopwordlist)
+def cleaning_stopwords(text):
+    return " ".join([word for word in str(text).split() if word not in STOPWORDS])
+dataset['text'] = dataset['text'].apply(lambda text: cleaning_stopwords(text))
+dataset['text'].head()
+data_bitcoin_work['text'] = data_bitcoin_work['text'].apply(lambda text: cleaning_stopwords(text))
+data_bitcoin_work['text'].tail()
+
+import string
+english_punctuations = string.punctuation
+punctuations_list = english_punctuations
+def cleaning_punctuations(text):
+    translator = str.maketrans('', '', punctuations_list)
+    return text.translate(translator)
+dataset['text']= dataset['text'].apply(lambda x: cleaning_punctuations(x))
+dataset['text'].tail()
+data_bitcoin_work['text']= data_bitcoin_work['text'].apply(lambda x: cleaning_punctuations(x))
+data_bitcoin_work['text'].tail()
+
+def cleaning_repeating_char(text):
+    return re.sub(r'(.)1+', r'1', text)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_repeating_char(x))
+dataset['text'].tail()
+data_bitcoin_work['text'] = data_bitcoin_work['text'].apply(lambda x: cleaning_repeating_char(x))
+data_bitcoin_work['text'].tail()
+
+def cleaning_URLs(data):
+    return re.sub('((www.[^s]+)|(https?://[^s]+))',' ',data)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_URLs(x))
+dataset['text'].tail()
+data_bitcoin_work['text'] = data_bitcoin_work['text'].apply(lambda x: cleaning_URLs(x))
+data_bitcoin_work['text'].tail()
+ 
+def cleaning_numbers(data):
+    return re.sub('[0-9]+', '', data)
+dataset['text'] = dataset['text'].apply(lambda x: cleaning_numbers(x))
+dataset['text'].tail()
+data_bitcoin_work['text'] = data_bitcoin_work['text'].apply(lambda x: cleaning_numbers(x))
+data_bitcoin_work['text'].tail()
+
+from nltk.tokenize import RegexpTokenizer
+tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|\S+')
+dataset['text'] = dataset['text'].apply(tokenizer.tokenize)
+dataset['text'].head()
+data_bitcoin_work['text'] = data_bitcoin_work['text'].apply(tokenizer.tokenize)
+data_bitcoin_work['text'].tail()
+
+import nltk
+st = nltk.PorterStemmer()
+nltk.download('omw-1.4')
+def stemming_on_text(data):
+    text = [st.stem(word) for word in data]
+    return data
+dataset['text']= dataset['text'].apply(lambda x: stemming_on_text(x))
+dataset['text'].head()
+data_bitcoin_work['text']= data_bitcoin_work['text'].apply(lambda x: stemming_on_text(x))
+data_bitcoin_work['text'].head()
+
+import nltk
+lm = nltk.WordNetLemmatizer()
+nltk.download('wordnet')
+def lemmatizer_on_text(data):
+    text = [lm.lemmatize(word) for word in data]
+    return data
+dataset['text'] = dataset['text'].apply(lambda x: lemmatizer_on_text(x)).apply(lambda x: ' '.join(x))
+dataset['text'].head()
+data_bitcoin_work['text'] = data_bitcoin_work['text'].apply(lambda x: lemmatizer_on_text(x)).apply(lambda x: ' '.join(x))
+data_bitcoin_work['text'].head()
+
+X=dataset.text
+y=dataset.target
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.15, random_state =42)
+
+vectoriser = TfidfVectorizer(ngram_range=(1,2), max_features=500000)
+vectoriser.fit(X_train)
+print('No. of feature_words: ', len(vectoriser.get_feature_names()))
+
+X_train = vectoriser.transform(X_train)
+X_test  = vectoriser.transform(X_test)
+X_sent = vectoriser.transform(data_bitcoin_work.text)
+
+def model_Evaluate(model):
+# Predict values for Test dataset
+    y_pred = model.predict(X_test)
+    # Print the evaluation metrics for the dataset.
+    print(classification_report(y_test, y_pred))
+    # Compute and plot the Confusion matrix
+    cf_matrix = confusion_matrix(y_test, y_pred)
+    categories = ['Negative','Positive']
+    group_names = ['True Neg','False Pos', 'False Neg','True Pos']
+    group_percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+    labels = [f'{v1}n{v2}' for v1, v2 in zip(group_names,group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cf_matrix, annot = labels, cmap = 'Blues',fmt = '',
+    xticklabels = categories, yticklabels = categories)
+    plt.xlabel("Predicted values", fontdict = {'size':14}, labelpad = 10)
+    plt.ylabel("Actual values" , fontdict = {'size':14}, labelpad = 10)
+    plt.title ("Confusion Matrix", fontdict = {'size':18}, pad = 20)
+
+LRmodel = LogisticRegression(C = 2, max_iter = 1000)
+LRmodel.fit(X_train, y_train)
+
+model_Evaluate(LRmodel)
+y_pred3 = LRmodel.predict(X_test)
+y_sent = LRmodel.predict(X_sent)
+btc_sent = pd.concat([data_bitcoin_work,pd.DataFrame(y_sent)],axis=1)
+btc_sent_daily = btc_sent.groupby(btc_sent.date).mean()*100
+
+from sklearn.metrics import roc_curve, auc
+fpr, tpr, thresholds = roc_curve(y_test, y_pred3)
+roc_auc = auc(fpr, tpr)
+plt.figure()
+plt.plot(fpr, tpr, color='darkorange', lw=1, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC CURVE')
+plt.legend(loc="lower right")
+plt.show()
